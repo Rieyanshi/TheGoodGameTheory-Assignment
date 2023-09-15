732dfff04d8639b6fd2039f5fe267f64c83a125e
Rieyanshi Sharma <86876634+Rieyanshi@users.noreply.github.com>
2023-09-15 22:45:27 +0530

    Added code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import BeerCard from './component/BeerCard';
 import './App.css';
 
 function App() {
+  const [beers, setBeers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    axios.get('https://api.punkapi.com/v2/beers').then((response) => {
+      setBeers(response.data);
+    });
+  }, []);
+
+  const filteredBeers = beers.filter((beer) =>
+    beer.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Beer Explorer</h1>
+      <input
+        type="text"
+        placeholder="Search for a beer..."
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <div className="beer-list">
+        {filteredBeers.map((beer) => (
+          <BeerCard key={beer.id} beer={beer} />
+        ))}
+      </div>
     </div>
   );
 }
